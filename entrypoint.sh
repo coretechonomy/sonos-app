aa45c472cdb7a027584f6d9c16c3767dd8cb9519
Erik Thomsen <ethomsen86@gmail.com>
2025-04-15 18:28:28 -0400

    Added new keypress

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 xvfb-run --auto-servernum --server-args="-screen 0 1024x768x16" bash -c \
     "wine /tmp/etechonomy/${SETUP_EXE} & \
     pid=\$! && while kill -0 \$pid 2> /dev/null; do \
+    xdotool key alt+i; \
     xdotool key alt+o; \
     sleep 5; \
     ls ~/.wine/drive_c/users/root/AppData/Local/Temp; \
