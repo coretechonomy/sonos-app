319f3cc9bfe67fb26e469e7b844da9d710baf0f6
Erik Thomsen <ethomsen86@gmail.com>
2025-04-15 18:39:45 -0400

    Using wildcard

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,9 @@ xvfb-run --auto-servernum --server-args="-screen 0 1024x768x16" bash -c \
     xdotool key alt+i; \
     xdotool key alt+o; \
     sleep 5; \
-    ls ~/.wine/drive_c/users/root/AppData/Local/Temp/{C33D420C-BD73-4BE3-ADA8-F6B7D46E63B0}; \
-    if [ -f ~/.wine/drive_c/users/root/AppData/Local/Temp/{C33D420C-BD73-4BE3-ADA8-F6B7D46E63B0}/${MSI_FILE} ]; then \
-        mv ~/.wine/drive_c/users/root/AppData/Local/Temp/{C33D420C-BD73-4BE3-ADA8-F6B7D46E63B0}/${MSI_FILE} /tmp/etechonomy/${MSI_FILE}; \
+    ls ~/.wine/drive_c/users/root/AppData/Local/Temp/*/${MSI_FILE}; \
+    if [ -f ~/.wine/drive_c/users/root/AppData/Local/Temp/*/${MSI_FILE} ]; then \
+        mv ~/.wine/drive_c/users/root/AppData/Local/Temp/*/${MSI_FILE} /tmp/etechonomy/${MSI_FILE}; \
         exit 0; \
     fi; \
     done && wait \$pid && echo 'Wine process finished'"
