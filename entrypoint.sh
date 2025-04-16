fd3329e499e906a758b239748df92d833c10e969
Erik Thomsen <ethomsen86@gmail.com>
2025-04-15 22:39:17 -0400

    Trying something different

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ set -e
 xvfb-run --auto-servernum --server-args="-screen 0 1024x768x16" bash -c \
     "wine /tmp/etechonomy/${SETUP_EXE} & \
     pid=\$! && while kill -0 \$pid 2> /dev/null; do \
-    xdotool key alt+i; \
+    for i in {1..5}; do xdotool key alt+i; sleep 1; done; \
     xdotool key alt+o; \
     sleep 5; \
-    ls ~/.wine/drive_c/users/root/AppData/Local/Temp/*/${MSI_FILE}; \
-    if [ -f ~/.wine/drive_c/users/root/AppData/Local/Temp/*/${MSI_FILE} ]; then \
-        mv ~/.wine/drive_c/users/root/AppData/Local/Temp/*/${MSI_FILE} /tmp/etechonomy/${MSI_FILE}; \
+    ls ~/.wine/drive_c/users/root/AppData/Local/Temp; \
+    if [ -f ~/.wine/drive_c/users/root/AppData/Local/Temp/${MSI_FILE} ]; then \
+        mv ~/.wine/drive_c/users/root/AppData/Local/Temp/${MSI_FILE} /tmp/etechonomy/${MSI_FILE}; \
         exit 0; \
     fi; \
     done && wait \$pid && echo 'Wine process finished'"
