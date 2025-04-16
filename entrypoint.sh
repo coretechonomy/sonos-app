25302625e969f9a097e8ae23147672e655c8baad
Erik Thomsen <ethomsen86@gmail.com>
2025-04-15 22:06:19 -0400

    Reverted

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,9 @@ set -e
 xvfb-run --auto-servernum --server-args="-screen 0 1024x768x16" bash -c \
     "wine /tmp/etechonomy/${SETUP_EXE} & \
     pid=\$! && while kill -0 \$pid 2> /dev/null; do \
-    sleep 1; \
     xdotool key alt+i; \
-    sleep 2; \
     xdotool key alt+o; \
-    sleep 2; \
-    ls ~/.wine/drive_c/users/root/AppData/Local/Temp/; \
-    ls ~/.wine/drive_c/users/root/AppData/Local/Temp/*; \
+    sleep 5; \
     ls ~/.wine/drive_c/users/root/AppData/Local/Temp/*/${MSI_FILE}; \
     if [ -f ~/.wine/drive_c/users/root/AppData/Local/Temp/*/${MSI_FILE} ]; then \
         mv ~/.wine/drive_c/users/root/AppData/Local/Temp/*/${MSI_FILE} /tmp/etechonomy/${MSI_FILE}; \
