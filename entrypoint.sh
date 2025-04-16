9f5b1acb4ac5b9ca0703058b3ca2a23d336a69e3
Erik Thomsen <ethomsen86@gmail.com>
2025-04-16 00:20:47 -0400

    Simplified Docker build and entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,11 @@ set -e
 xvfb-run --auto-servernum --server-args="-screen 0 1024x768x16" bash -c \
     "wine /tmp/etechonomy/${SETUP_EXE} & \
     pid=\$! && while kill -0 \$pid 2> /dev/null; do \
-    for i in {1..5}; do xdotool key alt+i; sleep 1; done; \
+    xdotool key alt+i; \
     xdotool key alt+o; \
-    sleep 5; \
-    ls ~/.wine/drive_c/users/root/AppData/Local/Temp; \
-    if [ -f ~/.wine/drive_c/users/root/AppData/Local/Temp/${MSI_FILE} ]; then \
-        mv ~/.wine/drive_c/users/root/AppData/Local/Temp/${MSI_FILE} /tmp/etechonomy/${MSI_FILE}; \
+    sleep 1; \
+    if [ -f ~/.wine/drive_c/users/root/AppData/Local/Temp/*/${MSI_FILE} ]; then \
+        mv ~/.wine/drive_c/users/root/AppData/Local/Temp/*/${MSI_FILE} /tmp/etechonomy/${MSI_FILE}; \
         exit 0; \
     fi; \
     done && wait \$pid && echo 'Wine process finished'"
